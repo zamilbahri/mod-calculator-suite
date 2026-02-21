06195f0a6f6b1d3de0c36c3f98671203027e4028
Zamil Bahri <zamilbahri@gmail.com>
2026-02-21 02:28:48 -0500

    added arePairwiseCoprime() to check if moduli are coprime

--- src/utils/numberTheory.ts
+++ src/utils/numberTheory.ts
@@ -33,6 +33,15 @@ export function gcd(a: bigint, b: bigint): bigint {
   return x;
 }
 
+export function arePairwiseCoprime(nums: bigint[]): boolean {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (gcd(nums[i], nums[j]) !== 1n) return false;
+    }
+  }
+  return true;
+}
+
 export function extendedGCD(a: bigint, b: bigint): EGCDResult {
   // Iterative extended Euclidean algorithm.
   let oldR = a;
