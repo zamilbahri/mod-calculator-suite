13d67cd13e2fb8b48a23a26cc69d0cc8ca78aa0d
Zamil Bahri <zamilbahri@gmail.com>
2026-04-04 19:20:20 -0400

    chore: set vertical layout breakpoint to xl

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App: React.FC = () => {
       <div className="max-w-screen-2xl mx-auto px-2 py-10">
         <Header />
 
-        <div className="mt-8 grid grid-cols-1 xxl:grid-cols-2 gap-6 items-start">
+        <div className="mt-8 grid grid-cols-1 xl:grid-cols-2 gap-6 items-start">
           {/* Left column: RSA and Primes, then Galois Fields */}
           <div className="flex flex-col gap-6">
             <CalculatorGroup title="RSA and Primes" defaultOpen>
